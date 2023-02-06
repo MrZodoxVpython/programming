2d809665071cb8454ecc283f2c2133bd04641b89
yudizlaberoz <73115122+yudizlaberoz@users.noreply.github.com>
2023-02-06 11:47:54 +0700

    Update loopingKasir.java

--- loopingKasir.java
+++ loopingKasir.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Kasir {
+public class loopingKasir {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int jumlahBelanja = 0;
